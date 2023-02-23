05c808832798755ccf78f0eb9ee286283c63b319
Aruzhan2004 <220191@astanait.edu.kz>
2023-02-23 13:05:55 +0600

    Patient update and Human abstract class

--- clinic/entities/Patient.java
+++ clinic/entities/Patient.java
@@ -1,20 +1,18 @@
 package clinic.entities;
 
-
-public class Patient {
+public class Patient extends Human {
     private Integer id;
-    private String name;
-    private String surname;
-    private Integer age;
 
     public Patient(String name, String surname, Integer age) {
-        setName(name);
-        setSurname(surname);
-        setAge(age);
+        super(name, surname, age);
     }
     public Patient(Integer id, String name, String surname, Integer age) {
         this(name, surname, age);
         this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     @Override
@@ -27,31 +25,4 @@ public class Patient {
                 '}';
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
